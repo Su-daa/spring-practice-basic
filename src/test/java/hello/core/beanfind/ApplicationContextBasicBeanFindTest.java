7baf1e158fcbfe9b363b30b2a0fba771a149c1fd
Su-daa <subin.kk019@gmail.com>
2023-10-30 17:31:48 +0900

    10.30 실습 내용: 섹션 4

---
+++ src/test/java/hello/core/beanfind/ApplicationContextBasicBeanFindTest.java
@@ -0,0 +1,35 @@
+package hello.core.beanfind;
+
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class ApplicationContextBasicBeanFindTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SameBeanConfig.class);
+
+    @Test
+    void findBeanByTypeDuplicate() {
+//        ac.getBean(MemberRepository.class);
+        Assertions.assertThrows(NoUniqueBeanDefinitionException.class,
+                () -> ac.getBean(MemberRepository.class));
+    }
+
+    @Configuration
+    static class SameBeanConfig{
+
+        @Bean
+        public MemberRepository memberRepository1() {
+            return new MemoryMemberRepository();
+        }
+
+        @Bean
+        public MemberRepository memberRepository2() {
+            return new MemoryMemberRepository();
+        }
+    }
+}
