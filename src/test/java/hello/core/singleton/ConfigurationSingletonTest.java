ea742a3fb4a97f0f6a94c0245765a6dcbf89ba78
Su-daa <subin.kk019@gmail.com>
2023-11-07 16:30:44 +0900

    11.07 실습 내용: 섹션 7

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -12,27 +12,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ConfigurationSingletonTest {
 
-    @Test
-    void configurationTest() {
-        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
-        MemberServiceImpl memberService = ac.getBean("memberService",
-                MemberServiceImpl.class);
-        OrderServiceImpl orderService = ac.getBean("orderService",
-                OrderServiceImpl.class);
-        MemberRepository memberRepository = ac.getBean("memberRepository",
-                MemberRepository.class);
-
-        //모두 같은 인스턴스를 참고하고 있다.
-        System.out.println("memberService -> memberRepository = " +
-                memberService.getMemberRepository());
-        System.out.println("orderService -> memberRepository = " +
-                orderService.getMemberRepository());
-        System.out.println("memberRepository = " + memberRepository);
-
-        //모두 같은 인스턴스를 참고하고 있다.
-        assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
-        assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
-    }
+//    @Test
+//    void configurationTest() {
+//        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+//        MemberServiceImpl memberService = ac.getBean("memberService",
+//                MemberServiceImpl.class);
+//        OrderServiceImpl orderService = ac.getBean("orderService",
+//                OrderServiceImpl.class);
+//        MemberRepository memberRepository = ac.getBean("memberRepository",
+//                MemberRepository.class);
+//
+//        //모두 같은 인스턴스를 참고하고 있다.
+//        System.out.println("memberService -> memberRepository = " +
+//                memberService.getMemberRepository());
+//        System.out.println("orderService -> memberRepository = " +
+//                orderService.getMemberRepository());
+//        System.out.println("memberRepository = " + memberRepository);
+//
+//        //모두 같은 인스턴스를 참고하고 있다.
+//        assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+//        assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+//    }
 
     @Test
     void configurationDeep() {
